a15d4d206b08654d6583bef5f89ad59e0e16f0f7
Mauricio <malarcon@3cinteractive.com>
2014-07-03 12:02:42 -0400

    Modified to use ElasticSearch 1.2.1 and aws plugin 2.1.1

--- attributes/deb.rb
+++ attributes/deb.rb
@@ -1,2 +1,2 @@
-default.elasticsearch[:deb_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.12.deb"
-default.elasticsearch[:deb_sha] = "13783b95b35e347a3471f22234c544ae50655b78"
+default.elasticsearch[:deb_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.1.deb"
+default.elasticsearch[:deb_sha] = "60ae2764782369aea2428e634eda092830832cd9"
